6f9e1686d3536424ac18ff8706ab5a25b76d7c5b
Nathaly Berríos <n.berriosgonzalez@gmail.com>
2026-08-23 21:57:27 -0400

    correccion fase 2

--- solucion.py
+++ solucion.py
@@ -1,30 +1,37 @@
+# solucion.py
+
 import json
 import os
 from tabulate import tabulate
 
 print("--- Sistema de Admisión Veterinaria ---")
 
-# Límite máximo de atenciones para el día
 CUPOS_TOTALES = 10 
 
-# Cupos
 mascotas_ingresadas = int(input("¿Cuántas mascotas ya han entrado hoy?: "))
 cupos = CUPOS_TOTALES - mascotas_ingresadas 
 
-# Datos de la mascota
 nombre = input("Nombre de la mascota: ")
-peso = int(input("Peso de la mascota (kilos): ")) 
+peso = int(input("Peso de la mascota (kilos): "))
 
-# Regla de decisión con if y elif
+# Variable para guardar el resultado final
+estado = ""
+
 if peso <= 0:
+    estado = "Dato Inválido"
     print(f"Error: El peso ingresado para {nombre} es inválido. Debe ser mayor a 0.")
-elif cupos <= 0:
-    print(f"Rechazado: Lo sentimos, ya no quedan cupos para atender a {nombre} hoy.")
 elif peso > 15:
+    estado = "Rechazo: Excede peso"
     print(f"Rechazado: {nombre} excede el peso máximo permitido de 15 kilos para los caniles.")
+elif cupos <= 0:
+    estado = "Rechazo: Sin cupos"
+    print(f"Rechazado: Lo sentimos, ya no quedan cupos para atender a {nombre} hoy.")
 elif peso <= 15 and cupos > 0:
+    estado = "Aceptado"
     print(f"Aceptado: {nombre} ha sido ingresado con éxito a la jornada.")
-    
+
+# --- FASE 2: GUARDAR EN JSON Y MOSTRAR TABLA ---
+
 registros = []
 
 # Revisamos si el archivo ya existe para no borrar los registros anteriores
