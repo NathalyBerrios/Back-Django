8e40fad33c06dae24ab93bbd8bf9dbf341d4cba4
Nathaly Berríos <n.berriosgonzalez@gmail.com>
2026-08-25 19:13:57 -0400

    vista

--- miproyecto/core/views.py
+++ miproyecto/core/views.py
@@ -1,3 +1,11 @@
+import json, os
 from django.shortcuts import render
 
-# Create your views here.
+def resumen(request):
+    registros = []
+    # Evita que la página se caiga si el JSON aún no existe
+    if os.path.exists("datos.json"):
+        with open("datos.json") as f:
+            registros = json.load(f)
+            
+    return render(request, "resumen.html", {"registros": registros})
